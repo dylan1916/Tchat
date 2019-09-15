ba5ded35bd445ec6a7ab7ac021d14241897a8efd
Tordjman <dylantordjman@MacBook-Air-de-Tordjman.local>
2019-09-15 15:57:55 +0200

    front et inscription en cours

---
+++ pages/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2 class="header">Home</h2>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus veritatis eius aliquam quam, minima, error nulla numquam, reprehenderit eveniet harum quis cupiditate ullam asperiores corrupti tempora! In tempore ipsam veritatis!
+    Possimus, debitis voluptatum eaque incidunt deleniti nemo odit excepturi, quis soluta nostrum exercitationem commodi! Minus dolore iste, tempora ipsa sed eos inventore unde vero eligendi enim deleniti tempore, corporis nam.
+    Perspiciatis consequatur consequuntur laudantium vero in! Accusantium assumenda corrupti nobis quidem in! Rem alias debitis impedit. Sapiente perspiciatis officia est facilis fugit nulla porro temporibus. Similique repudiandae facilis ullam. Ex.
+    Tempore ratione delectus, facere ullam quas cumque temporibus sapiente vitae vero architecto eum reprehenderit facilis illo accusantium molestias quia excepturi necessitatibus, ipsam explicabo non ex recusandae? Aliquam illum perspiciatis odio?
+    Pariatur a ducimus, nobis, qui excepturi minus molestias expedita omnis ipsum, quisquam sunt eum illum laboriosam ipsa tempore! Voluptas, ratione architecto laudantium asperiores dignissimos officia harum voluptatem suscipit dolores soluta.
+    Quas doloribus beatae necessitatibus animi cum quisquam eos veritatis ipsam eius aliquid! Aperiam labore alias tempore dolor ipsa consequuntur maiores accusantium, cumque omnis vitae facilis beatae ea expedita, quia voluptate?
+    Ut reiciendis totam a culpa. Vel ipsam earum deserunt ipsum, obcaecati excepturi voluptates quidem quo ipsa officia quis laboriosam quae dicta ea laudantium, ut at ratione necessitatibus aperiam cum eveniet!
+    Autem, accusamus. Nobis aperiam nostrum itaque, nulla perspiciatis voluptatibus maxime. Aliquam vel officia dolorum accusamus corporis nostrum, eos quaerat modi ullam quae maxime quo ipsam iusto delectus repellendus accusantium! Molestiae.
+    Possimus nam rerum voluptatem! Similique omnis distinctio fugit molestiae temporibus laborum ullam repellendus nostrum architecto minima labore soluta odio veniam, voluptas illo blanditiis fugiat in, ad adipisci dolorem assumenda totam?
+    Architecto, sequi sed itaque vel velit cum perspiciatis exercitationem minima molestias, magnam beatae! Mollitia, voluptatum quibusdam qui quo natus provident porro, ex laudantium nesciunt placeat consequuntur? Accusamus esse maiores voluptate.
+</body>
+</html>
---
+++ pages/register.php
@@ -0,0 +1,22 @@
+<h2 class="header header-form">S'inscrire</h2>
+
+<form method="POST" action="" id="regForm">
+
+    <div class="field">
+        <label for="name" class="field-label">Votre nom</label>
+        <input type="text" class="field-input" name="name" id="name">
+    </div>
+
+    <div class="field">
+        <label for="email" class="field-label">Votre adresse email</label>
+        <input type="email" class="field-input" name="email" id="email">
+    </div>
+
+    <div class="field">
+        <label for="password" class="field-label">Votre mot de passe</label>
+        <input type="password" class="field-input" name="password" id="password">
+    </div>
+
+    <button type="submit" name="submit">S'inscrire</button>
+
+</form>
